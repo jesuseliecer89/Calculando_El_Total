a8f595d5c71fbbff23ef11692dbfbd413685654f
Jesus Zuñiga <jesuseliecer89@gmail.com>
2024-11-05 22:11:08 -0500

    update all proyect

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Calculando_El_Total
 Desafio JavaScript: Calculando el total
+
+Trabajo realizado por:
+
+Gerardo Melendez Ponce
+Jesus Eliecer Zuñiga Meneses
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,41 @@
-precio = 400000
+const incrementar = document.querySelector('#incrementar')
+const decrementar = document.querySelector('#decrementar')
+let cantidad = document.querySelector('.cantidad')
+let total = document.querySelector('.valor-total')
 
+
+let valorCantidad = 0
+cantidad.innerHTML = valorCantidad
+
+precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+
+
+
+incrementar.onclick = function(){
+
+    cantidad.innerHTML = `${++valorCantidad}`
+
+    total.innerHTML = `${valorCantidad * precio}`
+
+}
+
+
+decrementar.onclick = function(){
+
+    if(valorCantidad >= 0){
+        cantidad.innerHTML = `${valorCantidad--}`
+        
+    }
+
+    if(valorCantidad >= 0 ){
+        total.innerHTML = `${valorCantidad * precio}`
+    }
+    
+}
+
+
+
+
